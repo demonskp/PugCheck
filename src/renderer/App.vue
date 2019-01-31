6190334ef23f8e92daab45c2bb88ef7fb0f7f956
易镇艺 <981577025@qq.com>
2019-01-31 15:31:29 +0800

    aa

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -11,15 +11,17 @@
 </script>
 
 <style>
-  html, body {
-        height: 100%;
-        margin: 0px;
-        padding: 0px;
-    }
-    #app{
-        height: 100%;
-        margin: 0px;
-        padding: 0px;
-        background-color:#FFDAB9;
-    }
+  html,
+  body {
+    height: 100%;
+    margin: 0px;
+    padding: 0px;
+  }
+
+  #app {
+    height: 100%;
+    margin: 0px;
+    padding: 0px;
+    background-color: #FFDAB9;
+  }
 </style>
