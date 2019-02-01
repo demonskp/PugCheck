c47c8df245717f8fe618159ebfb3677916ecf7a6
易镇艺 <981577025@qq.com>
2019-02-01 12:08:21 +0800

    增加税金核对确定所有发票项是否被核对

--- src/module/checkEarning.js
+++ src/module/checkEarning.js
@@ -5,6 +5,9 @@ const FILDNAME="税金收入核对表.xlsx";
 
 var checkEarning={
 
+/**
+ * 税金收入核对功能
+ */
 getAllDifference:(data)=>{
     
     var earnData=data[0].data;  //取得Sheet1当中的所有数据
@@ -16,67 +19,72 @@ getAllDifference:(data)=>{
     earnData.forEach(element => {
         sum=0.0;
         earnData.forEach(item=>{
-            if(element[0] instanceof String){}else{
+            if(element[0] instanceof String){
+
+            }else{
                 if(element[0]==item[0]){
                     sum+=item[1];
-                    
                 }
             }
-            
-            
         });
         element[2]=sum;
         if(element[4]!=null&&element[5]!=null){
             element[6]=element[4]/element[5];
         }
-        
-
-
     });
-
 
     //将税单凭证和 收入金额计算出的和(该税单下总收入金额)  存入 一个二维数组 itemArray
     var length=earnData.length;
-    var itemArray=[[],[]];
+    var itemArray=[[],[]];//核对数组 0、税单凭证号，1、该税单应交金额
     for(var i=0;i<length;i++){
        var earnId = earnData[i][3]; //凭证号
        var earnMoney=earnData[i][6]; //凭证单笔价格
-       if(earnId!=""&&itemArray[0].indexOf(earnId)<0){
+       var index1 = itemArray[0].indexOf(earnId);
+       if(earnId!=""&&index1<0){
            itemArray[0].push(earnId);
            itemArray[1].push(earnMoney);
        }else if(earnId!=""){
-           itemArray[1][itemArray[0].indexOf(earnId)]+=earnMoney;
+           itemArray[0][index1] = "re" + itemArray[0][index1];
+           itemArray[0].push(earnId);
+           itemArray[1].push(itemArray[1][index1]+earnMoney);
        }
-
     }
 
     //获取每一项凭证的差值
     var index=-1;
     earnData.forEach(item=>{
         index=itemArray[0].indexOf(item[0]);
+        console.log("index:"+index);
+        console.log("id:"+item[0]);
         if(item[0]!=""&&index>=0){
             item[8]=item[2] - itemArray[1][index];
+            earnData[index][7] = "此税单凭证号已被核对";
         }else if(item[0]!=""){
             item[8]=item[2];
         }
         
     })
-
-
     //console.log(earnData);
     earnData[0]=['收入凭证','收入金额','该凭证下总金额','税单凭证号','应交税费','税率','收入金额','','差额'];
     data[0].data=earnData;
     return data;
-
 },
 
-
+/**
+ * 将数据回写到Excel表格当中
+ * @param {输入的表格数据} jsonData 
+ * @param {文件生成的路径} path 
+ */
 writeExcel:function(jsonData,path){
     console.log("weiteExcel():"+jsonData[0].data[0]+"-"+path);
     var file = xlsx.build(jsonData);
     fs.writeFileSync(path,file);
 },
 
+/**
+ * 增值税核对主功能
+ * @param {需要核对的数据} data 
+ */
 taxCheck:function(data){
 
     var earnData=data[0].data;
@@ -108,8 +116,6 @@ taxCheck:function(data){
             lastDataArry[1].push(Number(element[3]));
             lastDataArry[2].push(false);
         }
-        
-
     }
 
     // console.log(firstDataArray);
--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -4,24 +4,22 @@
   </div>
 </template>
 
-<script>  
+<script>
   export default {
     name: 'mytest'
   }
 </script>
 
 <style>
-  html,
-  body {
-    height: 100%;
-    margin: 0px;
-    padding: 0px;
-  }
-
-  #app {
-    height: 100%;
-    margin: 0px;
-    padding: 0px;
-    background-color: #FFDAB9;
-  }
+  html, body {
+        height: 100%;
+        margin: 0px;
+        padding: 0px;
+    }
+    #app{
+        height: 100%;
+        margin: 0px;
+        padding: 0px;
+        background-color:#FFDAB9;
+    }
 </style>
